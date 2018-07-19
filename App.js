78b4cf083f418ac042b6884061051b058217fce4
Nelcifran <nelcifranpires@gmail.com>
2018-07-19 13:34:31 -0300

    api consuming

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header';
-
+import axios from 'axios';
 export default class App extends React.Component {
+  renderList(){
+
+    axios.get('https://randomuser.me/api/?nat=br&results=5')
+    .then(response =>{
+      const {results} = response.data;
+      const names = results.map(people =>people.name.first);
+      console.log(names);
+
+    })
+
+  }
+  
+  
   render() {
+    
     return (
       <View>
-        <Header />
+        <Header title="Easy Header!"/>
+        {this.renderList()}        
       </View>
       
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,StyleSheet } from 'react-native';
 
-const containerStyle = {
-	marginTop: 25,
-	backgroundColor: '#FFFFFF',
-
-}
-
-const titleStyle = {
-	fontSize:50
-}
 
 const Header = (props) =>(
-	<View style={containerStyle}>
-		<Text style={titleStyle}> Header aqui</Text>
+	<View style={style.container}>
+		<Text style={style.title}> {props.title}</Text>
 	</View>	
 	);
+
+
+const style = StyleSheet.create({
+	container: {
+		marginTop: 25,
+		backgroundColor: '#6ca2f7',
+	
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		fontSize: 50,
+		color: '#fff',
+	}
+});
+
 
 export default Header;
