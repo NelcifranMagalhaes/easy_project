2e38b4b29769bc66351a442a174c3f9f0631aa21
nelcifran <nelcifranpires@gmail.com>
2018-10-11 11:47:12 -0300

    Line component added and refactoring in PeopleDetailPage

--- src/pages/PeopleDetailPage.js
+++ src/pages/PeopleDetailPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {  Text, View ,Image,StyleSheet} from 'react-native';
+import Line from '../components/Line';
 
 export default class PeopleDetailPage extends React.Component{
 
@@ -8,13 +9,16 @@ export default class PeopleDetailPage extends React.Component{
         
         return (
             <View style={styles.container}>
-                        <Image style={styles.avatar}
+                <Image style={styles.avatar}
                         source={{uri: people.picture.large}}/>
                 <View style={styles.detailContainer}>
-                    <View style={styles.line}>
-                        <Text style={[styles.cell,styles.label]}>Email: </Text>
-                        <Text style={styles.cell}>{people.email}</Text>
-                    </View>
+                    <Line label = "Email: " content = {people.email} />
+                    <Line label = "Cidade: " content = {people.location.city} />
+                    <Line label = "Estado: " content = {people.location.state} />
+                    <Line label = "Tel: " content = {people.phone} />
+                    <Line label = "Cel: " content = {people.cell} />
+                    <Line label = "Nat: " content = {people.nat} />
+
                 </View>
             </View>
 
@@ -39,22 +43,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#e2f9ff',
         marginTop: 20,
         elevation: 1
-    },
-
-    line: {
-        flexDirection: 'row',
-        paddingTop: 3,
-        paddingBottom: 3,
-        borderWidth: 1,
-        borderColor: '#C5C5C5'
-    },
-
-    cell:{
-        fontSize: 18,
-        paddingLeft: 5,
-    },
-
-    label:{
-        fontWeight: 'bold',
     }
 });
